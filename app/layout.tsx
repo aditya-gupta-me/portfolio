f5b8b9d9b63b62293d75579a8750c8dcecb0778b
Aditya Gupta <adityagupta46772@gmail.com>
2026-07-29 23:28:40 +0530

    feat: change default theme from system to light

--- app/layout.tsx
+++ app/layout.tsx
@@ -122,7 +122,7 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(personSchema) }}
         />
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange>
           <a
             href="#main-content"
             className="sr-only focus:not-sr-only focus:fixed focus:top-4 focus:left-4 focus:z-50 focus:px-4 focus:py-2 focus:rounded-md focus:text-sm focus:font-medium focus:bg-black dark:focus:bg-white focus:text-white dark:focus:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
